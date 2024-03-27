d040d222c42a0f6a02f1318dd086c4433c963249
Amin <amin.mesmoudi@univ-poitiers.fr>
2024-03-27 04:28:35 +0100

    add load data script

--- SOHAD/build/master/scripts/loadData.sh
+++ SOHAD/build/master/scripts/loadData.sh
@@ -1,8 +1,20 @@
 #Transformer le fichier nt raw
+mkdir -p /data/tmp/$1 $2
+python3 /opt/replaceNS.py $1 /data/tmp/$1/$1
 
 #Charger dans le HDFS 
+hdfs dfs -put /data/tmp/$1/*.nt
 
 # Lancer le partitionneur de S2RDF
+mkdir /data/tmp/$1/stats
+python2 /opt/workspace/DataSetCreator/S2RDF_DataSetCreator/DataSetCreator.py -i /user/root/$1 -s $2
+mv stat-* /data/tmp/$1/data/tmp/$1/stats
+
+
+
+
+
+
 
 
 
---
+++ SOHAD/build/master/scripts/replaceNS.py
@@ -0,0 +1,36 @@
+# Mappings of full URIs to their abbreviations
+prefixes = {
+    "http://purl.org/dc/terms/": "dc:",
+    "http://xmlns.com/foaf/": "foaf:",
+    "http://purl.org/goodrelations/": "gr:",
+    "http://www.geonames.org/ontology#": "gn:",
+    "http://purl.org/ontology/mo/": "mo:",
+    "http://ogp.me/ns#": "og:",
+    "http://purl.org/stuff/rev#": "rev:",
+    "http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf:",
+    "http://www.w3.org/2000/01/rdf-schema#": "rdfs:",
+    "http://schema.org/": "sorg:",
+    "http://db.uwaterloo.ca/~galuc/wsdbm/": "wsdbm:"
+}
+
+# Function to replace prefixes in the second column
+def replace_prefixes(line):
+    attributes = line.split("\t")  # Assuming attributes are separated by tabs
+    second_attribute = attributes[1]  # Get the second attribute
+    for full, abbreviation in prefixes.items():
+        if full in second_attribute:
+            second_attribute = second_attribute.replace(full, abbreviation).replace("<", "").replace(">", "")
+            break  # Exit the loop after the first successful replacement
+    attributes[1] = second_attribute  # Update the second attribute
+    return "\t".join(attributes)  # Join the modified attributes with tabs
+
+# File paths
+source_file_path = 'watdiv.10M.nt'
+destination_file_path = 'watdiv.10M.v2.nt'
+
+# Read the source file, transform, and write the result to the destination file
+with open(source_file_path, 'r', encoding='utf-8') as source_file, \
+     open(destination_file_path, 'w', encoding='utf-8') as destination_file:
+    for line in source_file:
+        transformed_line = replace_prefixes(line.strip())  # .strip() to remove spaces and newline characters
+        destination_file.write(transformed_line + "\n")  # Add a newline after each transformed line
